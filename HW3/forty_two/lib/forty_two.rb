3210af5c57767027d215d899cf4d2e8d0515fc3a
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-14 20:34:35 +0200

    forty

---
+++ HW3/forty_two/lib/forty_two.rb
@@ -0,0 +1,12 @@
+require "forty_two/version"
+module FortyTwo
+  def self.forty_two?
+    p "HELLO"
+  end
+end
+FortyTwo.forty_two?
+# module FortyTwo
+#   def self.forty_two?
+#     to_i == 42 || to_i.to_s == 42
+#   end
+# end
---
+++ HW3/forty_two/lib/forty_two/version.rb
@@ -0,0 +1,3 @@
+module FortyTwo
+  VERSION = "0.1.0"
+end
