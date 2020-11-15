c7dc06adf7a7ebf332706c72813471542f42affa
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-15 20:35:42 +0200

    text_add

---
+++ HW3/text_add_html_file/lib/text_add_html_file.rb
@@ -0,0 +1,18 @@
+require "text_add_html_file/version"
+
+class AddText
+  def add_file(homepage)
+    out_file = File.open("index.html", "w")
+    out_file.puts "<!DOCTYPE>"
+    out_file.puts "<html lang='en'>"
+    out_file.puts "<head>"
+    out_file.puts "<title>hw3</title>"
+    out_file.puts "</head>"
+    out_file.puts "<body>"
+    out_file.puts "<h1>#{homepage}</h1>"
+    out_file.puts "<p>Hello guys</p>"
+    out_file.puts "</body>"
+    out_file.puts "</html>"
+    out_file.close
+  end
+end
