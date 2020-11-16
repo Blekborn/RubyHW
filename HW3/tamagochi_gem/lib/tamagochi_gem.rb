6f58fb429017cec08bf8b0e9cb48b0bd5a19212b
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-16 19:03:31 +0200

    tama_gem

---
+++ HW3/tamagochi_gem/lib/tamagochi_gem.rb
@@ -0,0 +1,6 @@
+require "tamagochi_gem/version"
+
+module TamagochiGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ HW3/tamagochi_gem/lib/tamagochi_gem/version.rb
@@ -0,0 +1,3 @@
+module TamagochiGem
+  VERSION = "0.1.0"
+end
