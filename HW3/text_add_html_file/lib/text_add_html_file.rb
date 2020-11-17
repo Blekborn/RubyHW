876792b82d820e122d0bb71d7d2b9465f1f5bdee
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-17 23:22:37 +0200

    h2

--- HW3/text_add_html_file/lib/text_add_html_file.rb
+++ HW3/text_add_html_file/lib/text_add_html_file.rb
@@ -1,19 +1,24 @@
 require "text_add_html_file/version"
+require 'erb'
 
-class AddText
-  def add_file(homepage)
-    out_file = File.open("index.html", "w")
-    out_file.puts "<!DOCTYPE html>"
-    out_file.puts "<html lang='en'>"
-    out_file.puts "<head>"
-    out_file.puts "<meta charset='UTF-8'>"
-    out_file.puts "<title>hw3</title>"
-    out_file.puts "</head>"
-    out_file.puts "<body>"
-    out_file.puts "<h1>#{homepage}</h1>"
-    out_file.puts "<p>Hello guys</p>"
-    out_file.puts "</body>"
-    out_file.puts "</html>"
-    out_file.close
+def add_file(content, file_name = 'index', bypass_html = false)
+  bypass_html ? content : content.gsub!(%r{<([^>]+?)([^>]*?)>(.*?)</\1>}, '')
+  output_file = File.expand_path("#{file_name}.html")
+  template =%{<!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>Content to HTML</title>
+          </head>
+          <body>
+            <%= @content %>
+          </body>
+          </html>
+          }
+  @content = content
+  file = ERB.new(template)
+  result = file.result
+  File.open(output_file, 'w') do |f|
+    f.write(result)
   end
 end
