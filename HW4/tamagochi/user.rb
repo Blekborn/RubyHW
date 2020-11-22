9e7fb5a3aac9b65f6f8b0579ea68a844f1a573c5
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-22 18:09:52 +0200

    user_a

--- HW4/tamagochi/user.rb
+++ HW4/tamagochi/user.rb
@@ -1,1 +1,32 @@
+class Users
+  attr_accessor :user
 
+  def initialize(*user)
+    @user = user
+      #super
+  end
+
+  SUPER_ADMIN = { 'login' => 'superadmin' }.freeze
+  ADMIN = { 'login' => 'admin' }.freeze
+  GUEST = { 'login' => 'guest' }.freeze
+
+  def user_admin
+    login_user
+    if @user == SUPER_ADMIN['login']
+      p 'superadmin'
+    elsif @user == ADMIN['login']
+      p 'Admin'
+    elsif @user == GUEST['login']
+      p 'quest'
+      com_pet
+    end
+  end
+
+  def login_user
+    p 'Введите логин!'
+    p 'superadmin, admin and guest'
+    @user = gets.chomp.downcase
+    p "Здравствуйте #{@user}"
+  end
+end
+Users.new.user_admin
