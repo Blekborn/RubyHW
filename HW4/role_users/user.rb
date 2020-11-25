fb8c39ce01509d6d45afb65244e367622180b9e5
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-25 18:52:36 +0200

    role_users info

--- HW4/role_users/user.rb
+++ HW4/role_users/user.rb
@@ -3,23 +3,30 @@ require 'yaml'
 
 class User
   # Some documentation for User
-  attr_accessor :user_login
+  attr_accessor :user_login #, :password_user
 
   def initialize(*user_login)
     @user_login = user_login
+    #@password_user = password_user
   end
 
   SUPER_ADMIN = { 'login' => 'superadmin', 'password' => '1111' }.freeze
   ADMIN = { 'login' => 'admin', 'password' => '2222' }.freeze
 
+
   def users_login
     in_user
     if @user_login == SUPER_ADMIN['login'] && @password_user == SUPER_ADMIN['password']
-      p 'superadmin'
+      p "Здравствуйте #{@user_login}"
+      File.open('info.yml', 'w') { |file| file.write(SUPER_ADMIN.to_yaml) }
     elsif @user_login == ADMIN['login'] && @password_user == ADMIN['password']
-      p 'Admin'
+      p "Здравствуйте #{@user_login}"
+      File.open('info.yml', 'w') { |file| file.write(ADMIN.to_yaml) }
+    elsif @user_login == '' && @password_user == ''
+      in_user
     else
-      p 'quest'
+      p "Здравствуйте #{@user_login}"
+      File.open('info.yml', 'w') { |file| file.write(ADMIN.to_yaml) }
     end
   end
 
@@ -29,7 +36,9 @@ class User
     @user_login = gets.chomp.downcase
     p 'password:'
     @password_user = gets.chomp
-    p "Здравствуйте #{@user_login}"
   end
 end
 User.new.users_login
+
+# recipe_from_yaml_file = YAML.load(File.read("info.yml"))
+# p recipe_from_yaml_file
