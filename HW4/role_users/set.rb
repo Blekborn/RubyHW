2133d42166b8c948ffb6154a9245713927478f9e
Blekborn <dmytrodatsenko10@gmail.com>
2020-11-27 22:22:12 +0200

    role_us

---
+++ HW4/role_users/set.rb
@@ -0,0 +1,40 @@
+
+require_relative 'user'
+require_relative 'admin'
+require_relative 'super_admin'
+
+class Session
+
+  def initialize(login, password)
+    @login = login
+    @password = password
+  end
+
+  def create_session
+    if admin?
+      Admin.new(@login, @password).log_in
+    elsif super_admin?
+      SuperAdmin.new(@login, @password).log_in
+    else
+      User.new(@login, @password).log_in
+    end
+  end
+
+  def save_pet_data(pet_data)
+    data = YAML.load_file 'db/users.yml'
+    user = data.detect { |hash| hash[:login] == @login }
+    user[:pet_data] = pet_data
+    data << user
+    File.open('db/users.yml', 'w') { |file| file.write(data.to_yaml) }
+  end
+
+  private
+
+  def admin?
+    @login == 'admin' && @password == 'admin'
+  end
+
+  def super_admin?
+    @login == 'superadmin' && @password == 'superadmin'
+  end
+end
--- HW4/role_users/user.rb
+++ HW4/role_users/user.rb
@@ -4,21 +4,40 @@ require 'yaml'
 
 # Some documentation for User
 class User
+  attr_accessor :login, :password, :role, :data
 
-  attr_accessor :user_login, :password_user, :role
-
-  def initialize
-    @user_login = user_login
-    @password_user = password_user
+  def initialize(login, password, role = self.class.to_s, data = [])
+    @login = login
+    @password = password
     @role = role
+    @data = data
   end
 
-  def users_login
-
+  def create
+    { login: @login, password: @password, role: @role, pet_data: @data }
   end
 
-  def in_user
-    
+  def log_in
+    users = db_load
+    user = create
+    save(users, user) unless present?(@login)
+    user
+  end
+
+  def save(users, user)
+    users << user
+    File.open('db/users.yml', 'w+') { |file| file.write(users.to_yaml) }
+  end
+
+  def db_load
+    YAML.load(File.open('db/users.yml', 'r'))
+  end
+
+  def present?(login)
+    db_load.any? { |u| u[:login] == login }
+  end
+
+  def valid_password?(login, password)
+    db_load.any? { |u| u[:login] == login && user[:password] == password }
   end
 end
-User.new.users_login
