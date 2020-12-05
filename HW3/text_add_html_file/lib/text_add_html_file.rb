1ea147b3d563b9472ca4cd16b296ea678afa3ab4
Blekborn <dmytrodatsenko10@gmail.com>
2020-12-05 19:41:16 +0200

    lasts commits

--- HW3/text_add_html_file/lib/text_add_html_file.rb
+++ HW3/text_add_html_file/lib/text_add_html_file.rb
@@ -2,19 +2,20 @@ require "text_add_html_file/version"
 require 'erb'
 
 def add_file(content, file_name = 'index', bypass_html = false)
-  bypass_html ? content : content.gsub!(%r{<([^>]+?)([^>]*?)>(.*?)</\1>}, '')
+  bypass_html ? content : content = CGI.escapeHTML(content)
   output_file = File.expand_path("#{file_name}.html")
-  template =%{<!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <title>Content to HTML</title>
-          </head>
-          <body>
-            <%= @content %>
-          </body>
-          </html>
-          }
+  template =
+  %{<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Content to HTML</title>
+  </head>
+  <body>
+    <%= @content %>
+  </body>
+  </html>
+  }
   @content = content
   file = ERB.new(template)
   result = file.result
